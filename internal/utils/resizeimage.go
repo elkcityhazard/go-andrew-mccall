a73e4534a32e014c7a5b09411e5884c91f1ec891
elkcityhazard <andrew.m.mccall@gmail.com>
2022-12-18 10:15:27 -0500

    image cropper

--- internal/utils/resizeimage.go
+++ internal/utils/resizeimage.go
@@ -6,6 +6,8 @@ import (
 	"image"
 	"image/jpeg"
 	"image/png"
+	"log"
+	"math"
 	"net/http"
 	"os"
 	"path"
@@ -15,14 +17,27 @@ import (
 	"golang.org/x/image/draw"
 )
 
+// SubImager type is created to use type assertion to cast SubImage to the image
+
+type SubImager interface {
+	SubImage(r image.Rectangle) image.Image
+}
+
 func (m *Tools) ResizeImage(file *UploadedFile, pathToFile string, user *models.User, w http.ResponseWriter, r *http.Request) error {
 
 	fmt.Println("User email: ", user.Email)
 
+	// Open up the damn file
+
 	input, _ := os.Open(pathToFile)
+	//	don't forget to defer closing it to avoid memory leak
 	defer input.Close()
 
+	// create a new image buffer
+
 	imageBuff := make([]byte, 512)
+
+	// read the original file into the image buffer so we can check what type of file it is
 
 	_, err := input.Read(imageBuff)
 
@@ -30,14 +45,26 @@ func (m *Tools) ResizeImage(file *UploadedFile, pathToFile string, user *models.
 		return err
 	}
 
+	// get the file type from the buffer
+
 	fileType := http.DetectContentType(imageBuff)
 
+	// create the output file
+
 	output, _ := os.Create(path.Join("./static/uploads/", user.Email, fmt.Sprintf("avatar-%s", filepath.Base(file.NewFileName))))
+
+	// defer closing the output file until we are done writing it and the function exits (avoid memory leak)
 	defer output.Close()
+
+	// seek the file back to the beginning or else we won't be able to write the whole file
 
 	input.Seek(0, 0)
 
+	// create a new image variable
+
 	var src image.Image
+
+	// determine if the original file was a png or jpeg before continuing
 
 	if strings.EqualFold(fileType, "image/png") {
 		src, _ = png.Decode(input)
@@ -46,10 +73,52 @@ func (m *Tools) ResizeImage(file *UploadedFile, pathToFile string, user *models.
 		src, _ = jpeg.Decode(input)
 	}
 
-	fmt.Println(fileType)
+	// create a whole new sized image
 
 	// Set the expected size that you want:
-	dst := image.NewRGBA(image.Rect(0, 0, src.Bounds().Max.X/5, src.Bounds().Max.Y/5))
+	dst := image.NewRGBA(image.Rect(0, 0, src.Bounds().Max.X/4, src.Bounds().Max.Y/4))
+
+	// At returns the color of the pixel at (x, y).
+	// At(Bounds().Min.X, Bounds().Min.Y) returns the upper-left pixel of the grid.
+	// At(Bounds().Max.X-1, Bounds().Max.Y-1) returns the lower-right one.
+
+	bounds := dst.Bounds()
+
+	//	Get Width
+
+	width := bounds.Dx()
+
+	height := bounds.Dy()
+
+	// created the cropped size of the image
+
+	cropSize := image.Rect(0, 0, width/2, width/2)
+
+	// dynamically get focal point based on original width
+
+	focalX := math.Floor(float64(width / 2))
+	focalY := math.Floor(float64(height / 2))
+
+	//This is the place of the left and top padding of image that you want to crop. In this case we add padding left to 100 pixels and padding top of 80 pixel
+
+	cropSize = cropSize.Add(image.Point{int(focalX), int(focalY)})
+
+	//SubImage returns an image representing the portion of the image p visible through r. The returned value shares pixels with the original image.
+
+	croppedImage := src.(SubImager).SubImage(cropSize)
+
+	croppedImageFile, err := os.Create("./static/uploads/cropped.png")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer croppedImageFile.Close()
+
+	if err := png.Encode(croppedImageFile, croppedImage); err != nil {
+		log.Fatalln(err)
+		return err
+	}
 
 	// Resize:
 	draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
